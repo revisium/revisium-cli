447aa8e8ce58d9d01f9cd9986f178f1143f571bc
anton62k <anton.kashirov@gmail.com>
2026-01-03 19:05:01 +0300

    feat: remove patches

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ApplyMigrationsCommand } from 'src/commands/apply-migrations.command';
 import { CreateMigrationsCommand } from 'src/commands/create-migrations.command';
 import { MigrationCommand } from 'src/commands/migration.command';
-import { PatchesCommand } from 'src/commands/patches.command';
 import { RowsCommand } from 'src/commands/rows.command';
 import { SaveMigrationsCommand } from 'src/commands/save-migrations.command';
-import { SavePatchesCommand } from 'src/commands/save-patches.command';
 import { SaveRowsCommand } from 'src/commands/save-rows.command';
 import { SaveSchemaCommand } from 'src/commands/save-schema.command';
 import { SchemaCommand } from 'src/commands/schema.command';
@@ -15,18 +13,11 @@ import { SyncSchemaCommand } from 'src/commands/sync-schema.command';
 import { SyncDataCommand } from 'src/commands/sync-data.command';
 import { SyncAllCommand } from 'src/commands/sync-all.command';
 import { UploadRowsCommand } from 'src/commands/upload-rows.command';
-import { ValidatePatchesCommand } from 'src/commands/validate-patches.command';
-import { PreviewPatchesCommand } from 'src/commands/preview-patches.command';
-import { ApplyPatchesCommand } from 'src/commands/apply-patches.command';
 import { CommitRevisionService } from 'src/services/commit-revision.service';
 import { ConnectionFactoryService } from 'src/services/connection-factory.service';
 import { ConnectionService } from 'src/services/connection.service';
 import { InteractiveService } from 'src/services/interactive.service';
 import { JsonValidatorService } from 'src/services/json-validator.service';
-import { PatchDiffService } from 'src/services/patch-diff.service';
-import { PatchGeneratorService } from 'src/services/patch-generator.service';
-import { PatchLoaderService } from 'src/services/patch-loader.service';
-import { PatchValidationService } from 'src/services/patch-validation.service';
 import { SyncApiService } from 'src/services/sync-api.service';
 import { SyncSchemaService } from 'src/services/sync-schema.service';
 import { SyncDataService } from 'src/services/sync-data.service';
@@ -56,11 +47,6 @@ import {
     RowsCommand,
     SaveRowsCommand,
     UploadRowsCommand,
-    PatchesCommand,
-    SavePatchesCommand,
-    ValidatePatchesCommand,
-    PreviewPatchesCommand,
-    ApplyPatchesCommand,
     SyncCommand,
     SyncSchemaCommand,
     SyncDataCommand,
@@ -76,10 +62,6 @@ import {
     SyncDataService,
     RowSyncService,
     UrlBuilderService,
-    PatchLoaderService,
-    PatchValidationService,
-    PatchGeneratorService,
-    PatchDiffService,
   ],
 })
 export class AppModule {}
